412443e5563b30c6f8fb22bc18e2c8e33d8e0268
matt venn <matt@mattvenn.net>
2018-01-19 18:48:37 +0100

    show and hide tickboxes, export button

--- main.py
+++ main.py
@@ -17,48 +17,68 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
 
-        # load the image without having to faff with menus
-        self.image = QPixmap("./fade.jpg")
-        self.imageLabel.setPixmap(self.image)
-        self.imageLabel.setFixedSize(self.image.size());
-
-        self.spiraler = Spiraler(self.tab_2, self.image)
-        self.spiraler.setGeometry(QtCore.QRect(0, 0, 781, 431))
-        self.spiralLabel.setObjectName("spiraler")
+        # custom widget to show the spiral
+        self.spiraler = Spiraler(self)
 
         # hook menus
         self.actionOpen.triggered.connect(self.openFileDialog)
         self.actionSave.triggered.connect(self.saveFileDialog)
 
         # slots
-        self.horizontalSlider.valueChanged.connect(self.spiraler.updateDensity)
-        self.horizontalSlider_2.valueChanged.connect(self.spiraler.updateAmpScale)
-        self.horizontalSlider_3.valueChanged.connect(self.spiraler.updateDist)
+        self.densitySlider.valueChanged.connect(self.spiraler.updateDensity)
+        self.sensSlider.valueChanged.connect(self.spiraler.updateAmpScale)
+        self.distSlider.valueChanged.connect(self.spiraler.updateDist)
 
-        self.horizontalSlider.setValue(100)
-        self.horizontalSlider_2.setValue(20)
-        self.horizontalSlider_3.setValue(30)
-        #self.saveSvg()
-    
+        self.densitySlider.sliderReleased.connect(self.update)
+        self.sensSlider.sliderReleased.connect(self.update)
+        self.distSlider.sliderReleased.connect(self.update)
+
+        self.densitySlider.setValue(100)
+        self.sensSlider.setValue(20)
+        self.distSlider.setValue(30)
+
+        self.exportSVG.clicked.connect(self.saveSvg)
+
+        self.showSpiral.clicked.connect(self.spiraler.updateShow)
+        self.showImage.clicked.connect(self.updateShow)
+
+        # load the image without having to faff with menus
+        image = QPixmap("./fade.jpg")
+        self.updateImage(image)
+
+   
+    def updateShow(self, value):
+        if value:
+            self.imageLabel.setPixmap(self.image)
+        else:
+            self.imageLabel.clear()
+        
     def openFileDialog(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getOpenFileName(None,"QFileDialog.getOpenFileName()", "","Image Files (*.jpg)", options=options)
         if fileName:
-            self.image = QPixmap(fileName)
-            if self.image.isNull():
+            image = QPixmap(fileName)
+            if image.isNull():
                 QMessageBox.information(self, "Image Viewer", "Cannot load %s." % fileName)
                 return
 
-            # can't get scaling to work - oh well.
-            #self.image = self.image.scaledToWidth(self.tab_2.width())
-            #image = image.scaled(self.tab_2.width(), self.tab_2.height())
-            self.imageLabel.setPixmap(self.image)
-            self.imageLabel.setFixedSize(self.image.size());
+            self.updateImage(image)
+    
+    def updateImage(self, image):
+        self.image = image
 
-            self.spiraler.updateImage(self.image)
-            #self.process_image()
-            #self.imageLabel.setScaledContents(True)
+        # can't get scaling to work - oh well.
+        #self.image = self.image.scaledToWidth(self.tab_2.width())
+        #image = image.scaled(self.tab_2.width(), self.tab_2.height())
+
+        #self.imageLabel.setScaledContents(True)
+
+        self.imageLabel.setPixmap(self.image)
+
+        self.spiraler.setGeometry(QtCore.QRect(0, 0, self.image.size().width(), self.image.size().height()))
+
+        self.spiraler.updateImage(self.image)
 
 
     def saveSvg(self):
@@ -67,24 +87,27 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
         generator = QSvgGenerator()
         generator.setFileName(path)
-        generator.setSize(QSize(200, 200))
-        generator.setViewBox(QRect(0, 0, 200, 200))
-        generator.setTitle("SVG Generator Example Drawing")
-        generator.setDescription("An SVG drawing created by the SVG Generator")
+        w = self.image.size().width()
+        h = self.image.size().height()
+        generator.setSize(QSize(w, h))
+        generator.setViewBox(QRect(0, 0, w, h))
+        generator.setTitle("Spiraliser!")
         qp = QPainter(generator)
         self.spiraler.paint(qp)
         qp.end()
+        self.statusBar.showMessage("SVG exported to %s" % (path), 1000)
 
     def saveFileDialog(self):    
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
         fileName, _ = QFileDialog.getSaveFileName(None,"QFileDialog.getSaveFileName()","","All Files (*);;Text Files (*.txt)", options=options)
         if fileName:
+        print("resize")
             print(fileName)
 
     def resizeEvent(self, event):
         #self.image = self.image.scaledToWidth(self.imageLabel.width())
-        print("resize")
+        pass
 
 if __name__ == "__main__":
 
---
+++ spiral.py
@@ -0,0 +1,140 @@
+from math import cos, sin, radians, sqrt
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QMainWindow,QApplication, QPushButton, QTextEdit, QVBoxLayout, QWidget , QButtonGroup, QFileDialog
+from PyQt5.QtGui import QPainter, QImage, QPainterPath
+from PyQt5.QtGui import QPixmap, QColor, QPen, qGray, QPolygon
+from PyQt5.QtSvg import QSvgGenerator
+from PyQt5.QtCore import Qt, QSize, QRect, QPoint, pyqtSlot
+import time
+
+def remap(OldValue, OldMin, OldMax, NewMin, NewMax):
+    OldRange = (OldMax - OldMin)  
+    NewRange = (NewMax - NewMin)  
+    NewValue = (((OldValue - OldMin) * NewRange) / OldRange) + NewMin
+    return NewValue
+
+class Spiraler(QtWidgets.QLabel):
+
+    def __init__(self, parent):
+        super(Spiraler, self).__init__(parent=parent)
+        self.parent = parent
+
+    # maybe some good stuff here; https://github.com/baoboa/pyqt5/blob/master/examples/painting/svgviewer/svgviewer.py
+    # also intertesting pixelator example here: http://doc.qt.io/qt-5/qtwidgets-itemviews-pixelator-example.html
+
+    # svg generator example: http://doc.qt.io/qt-5/qtsvg-svggenerator-example.html
+    def paintEvent(self, event):
+        qp = QPainter(self)
+        self.paint(qp)
+        qp.end()
+
+    def updateImage(self, image):
+        self.image = image.toImage() # convert to image
+
+    # slider update methods - really have to repeat all this?
+    def updateDensity(self, value):
+        self.density = value
+
+    def updateAmpScale(self, value):
+        self.ampScale = value
+
+    def updateDist(self, value):
+        self.dist = value
+
+    def updateShow(self, value):
+        self.show = value
+        self.update()
+
+    @pyqtSlot(int)
+    def get_slider_value(self, val):
+        self.density = val
+        print("update density to %d" % val)
+
+    # this code is translated from java to python from https://github.com/krummrey/SpiralFromImage
+    def paint(self, qp):
+        
+        if not self.show:
+            return
+
+        density = self.density
+        dist = self.dist
+        ampScale = self.ampScale
+        start_time = time.time()
+
+        radius = 1 # variable for the radius 
+        alpha = 0 # angle of current point
+        aradius = 0
+        qp.setRenderHint(QPainter.Antialiasing)
+
+        # Calculates the first point
+        # currently just the center
+        # TODO: create button to set center with mouse
+        k = density/radius 
+        alpha += k
+        radius += dist/(360/k)
+        x =  aradius*cos(radians(alpha))+self.image.width()/2
+        y = -aradius*sin(radians(alpha))+self.image.height()/2
+        samples = 0
+
+        # when have we reached the far corner of the image?
+        # TODO: this will have to change if not centered
+        endRadius = sqrt(pow((self.image.width()/2), 2)+pow((self.image.height()/2), 2))
+
+        shapeOn = False
+        points = QPolygon()
+
+        # Have we reached the far corner of the image?
+        while radius < endRadius:
+            k = (density/2)/radius 
+            alpha += k
+            radius += dist/(360/k)
+
+            x =  radius*cos(radians(alpha))+self.image.width()/2
+            y = -radius*sin(radians(alpha))+self.image.height()/2
+
+            # Are we within the the image?
+            # If so check if the shape is open. If not, open it
+            if ((x>=0) and (x<self.image.width()-1) and (y>0) and (y<self.image.height())):
+                samples += 1
+                # Get the color and brightness of the sampled pixel
+                c = self.image.pixel(int(x), int(y))
+                b = qGray(c)
+                b = remap(b, 0, 255, dist*ampScale, 0)
+
+                # Move up according to sampled brightness
+                aradius = radius+(b/dist)
+                xa =  aradius*cos(radians(alpha))+self.image.width()/2
+                ya = -aradius*sin(radians(alpha))+self.image.height()/2
+
+                # Move around depending on density
+                k = (density/2)/radius 
+                alpha += k
+                radius += dist/(360/k)
+
+                # Move down according to sampled brightness
+                bradius = radius-(b/dist)
+                xb =  bradius*cos(radians(alpha))+self.image.width()/2
+                yb = -bradius*sin(radians(alpha))+self.image.height()/2
+
+                # Add vertices to shape
+                if (shapeOn == False) :
+                    shapeOn = True
+                
+                points.append(QPoint(xa, ya))
+                points.append(QPoint(xb, yb))
+
+            else:
+                # We are outside of the image so close the shape if it is open
+                if shapeOn == True:
+                    qp.drawPolyline(points)
+                    points.clear()
+                    shapeOn = False
+            
+          
+        if shapeOn:
+            qp.drawPolyline(points)
+            points.clear()
+
+        process_time = time.time() - start_time
+        self.parent.statusBar.showMessage("finished in %f secs, took %d samples" % (process_time, samples), 1000)
+
